ab620bf95463904832e0b9b3160660adf0ea5a5c
AbbasAli-R <abbas.ali252001@gmail.com>
2025-09-28 12:21:38 +0530

    Fixed import path issue for projectsData

--- src/sections/Marquee.jsx
+++ src/sections/Marquee.jsx
@@ -7,23 +7,23 @@ const Marquee = () => {
     {/* row 1 */}
     <div className='whitespace-nowrap animate-marquee text-6xl lg:text-[7vw] font-heading font-semibold leading-[1] tracking-tight'>
       <span className='flex gap-4 lg:gap-8 mx-8'>
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        CODE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        BUILD <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        DEPLOY <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        CODE <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        BUILD <img src={star} alt="" className='marquee-star1 w-[7vw]' />
+        DEPLOY <img src={star} alt="" className='marquee-star1 w-[7vw]' />
       </span>
     </div>
     {/* row 2 */}
       <div className='whitespace-nowrap animate-marquee-reverse text-6xl lg:text-[7vw] font-heading font-semibold leading-[1] tracking-tight'>
       <span className='flex gap-4 lg:gap-8 mx-8'>
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
-        CREATE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        CODE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        BUILD <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        DEPLOY <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        CODE <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        BUILD <img src={star} alt="" className='marquee-star2 w-[7vw]' />
+        DEPLOY <img src={star} alt="" className='marquee-star2 w-[7vw]' />
       </span>
     </div>
    </div>
